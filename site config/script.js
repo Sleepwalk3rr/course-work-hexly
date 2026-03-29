1179f662cad48cd57df842f9b4a31e332bb74df1
Sleepwalk3r <sad.toy.channel@gmail.com>
2026-03-29 18:16:52 +0300

    save code

--- site config/script.js
+++ site config/script.js
@@ -1,3 +1,20 @@
-const inputName = document.querySelector('#inputName')
+const inputName = document.getElementById('input-name')
 const inputPages = document.querySelector('#inputPages')
 const list = document.querySelector('#bookList')
+const btn = document.getElementById('add-button')
+
+btn.addEventListener('click', () => {
+    const text = inputName.value
+
+    if (text !== "") {
+        const li = document.createElement('li');
+        li.textContent = text;
+        list.appendChild(li)
+
+        inputName.value = "";
+    }
+})
+
+inputName.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') btn.click();
+});
