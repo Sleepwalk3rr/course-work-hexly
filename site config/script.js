6e940cf0b91aea51f6d6c683ddb3112cf573fed8
Sleepwalk3r <sad.toy.channel@gmail.com>
2026-04-08 18:40:09 +0300

    animations and statistic

--- site config/script.js
+++ site config/script.js
@@ -9,13 +9,15 @@ let items = JSON.parse(localStorage.getItem('myBooks')) || [];
 function render() {
   container.innerHTML = '';
 
+  updateStatistics();
+
   items.forEach((item, index) => {
     const card = document.createElement('div');
     card.className = 'card';
 
 card.innerHTML = `
   <div class="card-top-row">
-    <span class='cardName'><strong>Название:</strong> ${item.name}</span>
+    <span class='cardName'><strong>Книга:</strong> ${item.name}</span>
     <span class='cardReaden'><strong>Прочитано:</strong> ${item.readPages} из ${item.pages}</span>
     <span class='cardAuthor'><strong>Автор:</strong> ${item.author || 'Не указан'}</span>
   </div>
@@ -33,23 +35,32 @@ card.innerHTML = `
       <option value="on-hold" ${item.status === 'on-hold' ? 'selected' : ''}>Отложил</option>
     </select>
   </div>
-  </div class="card-bottom">
+  <div class="card-bottom">
     <button onclick="editItem(${index})" class="editButton">Редактировать</button>
     <button onclick="deleteItem(${index})" class="deleteButton">Удалить</button>
   </div>
 `;
 
+
+// Функция смены статуса
+window.updateStatus = (index, newStatus) => {
+    items[index].status = newStatus;
+    
+    if (newStatus === 'finished') {
+        items[index].readPages = items[index].pages; // Устанавливаем макс. страницы
+    }
+    
+    saveAndRender(); // Перерисовывает всё, включая статистику
+};
+
 window.updatePages = (index) => {
-  const newRead = prompt("Сколько страниц прочитано?", items[index].readPages || 0);
+  const newRead = prompt(`Сколько страниц прочитано? (Всего: ${items[index].pages})`, items[index].readPages || 0);
   if (newRead !== null) {
     const val = parseInt(newRead);
-    if (!isNaN(val) && val <= items[index].pages) {
+    if (!isNaN(val) && val <= items[index].pages && val >= 0) {
       items[index].readPages = val;
-      // Если прочитаны все страницы, ставим статус "Закончил"
       if (val === items[index].pages) items[index].status = 'finished';
       saveAndRender();
-    } else {
-      alert("Введите корректное число (не больше общего кол-ва страниц)");
     }
   }
 };
@@ -124,3 +135,15 @@ btn.addEventListener('click', () => {
 });
 
 render();
+
+
+function updateStatistics() {
+    const finishedCountEl = document.getElementById('finished-count');
+    if (!finishedCountEl) return;
+
+    // Считаем книги
+    const finishedBooks = items.filter(book => book.status === 'finished');
+
+    // Обновляем текст на странице
+    finishedCountEl.innerText = finishedBooks.length;
+}
